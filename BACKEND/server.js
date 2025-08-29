e90309ce7a229729c6f97c8d8943ac375210445d
SOrtizRamirez <ortizramirezs@gmail.com>
2025-08-29 16:29:27 -0500

    Cambio base de datos(Variables de entorno)

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const cors = require('cors');
-const patientRoutes = require('./routes/patients.js');
-const authRoutes = require('./routes/auth.js');
-const publicRoutes = require('./routes/public.js');
-const doctorRoutes = require('./routes/doctor.js');
-const emergencyContactsRoutes = require('./routes/emergency_contacts.js');
+const patientRoutes = require('./src/routes/patients.js');
+const authRoutes = require('./src/routes/auth.js');
+const publicRoutes = require('./src/routes/public.js');
+const doctorRoutes = require('./src/routes/doctor.js');
+const emergencyContactsRoutes = require('./src/routes/emergency_contacts.js');
 const bcrypt = require('bcrypt');
-const pool = require('../src/config/db.js');
+const pool = require('./src/config/db.js');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use('/auth', require('./routes/auth'));
+app.use('/auth', require('./src/routes/auth'));
 
 app.get('/test-db', async (req, res) => {
     try {
-        const pool = require('./config/db');
+        const pool = require('./src/config/db');
         const [rows] = await pool.query('SELECT NOW() AS now');
         res.json({
             status: 'success',
@@ -33,10 +33,10 @@ app.get('/test-db', async (req, res) => {
 });
 
 
-app.use('/patients', require('./routes/patients'));
-app.use('/public', require('./routes/public'));
-app.use('/doctor', require('./routes/doctor'));
-app.use('/emergency-contacts', require('./routes/emergency_contacts'));
+app.use('/patients', require('./src/routes/patients'));
+app.use('/public', require('./src/routes/public'));
+app.use('/doctor', require('./src/routes/doctor'));
+app.use('/emergency-contacts', require('./src/routes/emergency_contacts'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
